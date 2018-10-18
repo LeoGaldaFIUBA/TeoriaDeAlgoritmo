b06677ece93886cd362d09b617984e2c411e20e9
LeoGalda <leonardo.galdames@hotmail.com>
2018-10-18 17:38:28 -0300

    Division y conquista pseudoTerminado

--- RobotSeguro/DivisionYConquista.cpp
+++ RobotSeguro/DivisionYConquista.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <cmath>
 
-#define MAXIMO_DE_PUNTOS 4
+#define MAXIMO_DE_PUNTOS 3
 
 DivisionYConquista::DivisionYConquista() {
 }
@@ -16,119 +16,89 @@ int DivisionYConquista::cortaElPoligono(Punto a, Punto b, Punto c) {
             (c.getY() - b.getY()) * (b.getX() - a.getX()));
 }
 
-std::vector<Punto> DivisionYConquista::mergearEnvolturas(Puntos izq, Puntos der) {
-    int tamanioIzq = izq.size();
-    int tamanioDer = der.size();
-
-    int posMasDerechaDeIzqDeX = 0, posMasIzquierdaDeDerDeX = 0;
-    for (int i = 1; i < tamanioIzq; i++) {
-        if (izq[i].getX() > izq[posMasDerechaDeIzqDeX].getX()) {
-            posMasDerechaDeIzqDeX = i;
+int DivisionYConquista::getPosicionMayorX(Puntos puntos) {
+    int mayor = 0;
+    for (int i = 1; i < (int) puntos.size(); i++) {
+        if (puntos[i].getX() > puntos[mayor].getX()) {
+            mayor = i;
         }
     }
-    for (int i = 1; i < tamanioDer; i++) {
-        if (der[i].getX() < der[posMasIzquierdaDeDerDeX].getX()) {
-            posMasIzquierdaDeDerDeX = i;
+    return mayor;
+}
+
+int DivisionYConquista::getPosicionMenorX(Puntos puntos) {
+    int menor = 0;
+    for (int i = 1; i < (int) puntos.size(); i++) {
+        if (puntos[i].getX() < puntos[menor].getX()) {
+            menor = i;
         }
     }
+    return menor;
+}
 
-
-    //-----------------------------------------------------------------------------    
-    int inda = posMasDerechaDeIzqDeX, indb = posMasIzquierdaDeDerDeX;
+void DivisionYConquista::hallarUpper(Puntos izq, Puntos der, int& upperIzq, int& upperDer) {
     bool done = 0;
+    int tamanioIzq = (int) izq.size();
+    int tamanioDer = (int) der.size();
     while (!done) {
         done = 1;
-        while (this->cortaElPoligono(der[indb], izq[inda], izq[(inda + 1) % tamanioIzq]) >= 0) {
-            inda = (inda + 1) % tamanioIzq;
+        while (this->cortaElPoligono(der[upperDer], izq[upperIzq], izq[(upperIzq + 1) % tamanioIzq]) >= 0) {
+            upperIzq = (upperIzq + 1) % tamanioIzq;
         }
-        while (this->cortaElPoligono(izq[inda], der[indb], der[(tamanioDer + indb - 1) % tamanioDer]) <= 0) {
-            indb = (tamanioDer + indb - 1) % tamanioDer;
+        while (this->cortaElPoligono(izq[upperIzq], der[upperDer], der[(tamanioDer + upperDer - 1) % tamanioDer]) <= 0) {
+            upperDer = (tamanioDer + upperDer - 1) % tamanioDer;
             done = 0;
         }
     }
-    int uppera = inda, upperb = indb;
-    //-----------------------------------------------------------------------------
-    inda = posMasDerechaDeIzqDeX;
-    indb = posMasIzquierdaDeDerDeX;
-    done = 0;
+}
+
+void DivisionYConquista::hallarLower(Puntos izq, Puntos der, int& lowerIzq, int& lowerDer) {
+    bool done = 0;
+    int tamanioIzq = (int) izq.size();
+    int tamanioDer = (int) der.size();
     while (!done) {
         done = 1;
-        while (this->cortaElPoligono(izq[inda], der[indb], der[(indb + 1) % tamanioDer]) >= 0)
-            indb = (indb + 1) % tamanioDer;
-        while (this->cortaElPoligono(der[indb], izq[inda], izq[(tamanioIzq + inda - 1) % tamanioIzq]) <= 0) {
-            inda = (tamanioIzq + inda - 1) % tamanioIzq;
+        while (this->cortaElPoligono(izq[lowerIzq], der[lowerDer], der[(lowerDer + 1) % tamanioDer]) >= 0) {
+            lowerDer = (lowerDer + 1) % tamanioDer;
+        }
+        while (this->cortaElPoligono(der[lowerDer], izq[lowerIzq], izq[(tamanioIzq + lowerIzq - 1) % tamanioIzq]) <= 0) {
+            lowerIzq = (tamanioIzq + lowerIzq - 1) % tamanioIzq;
             done = 0;
         }
     }
-    int lowera = inda, lowerb = indb;
-    //-----------------------------------------------------------------------------
+}
+
+std::vector<Punto> DivisionYConquista::mergearEnvolturas(Puntos izq, Puntos der) {
+    int posMasDerechaDeIzqDeX = this->getPosicionMayorX(izq);
+    int posMasIzquierdaDeDerDeX = this->getPosicionMenorX(der);
+
+    int upperIzquierda = posMasDerechaDeIzqDeX;
+    int upperDerecha = posMasIzquierdaDeDerDeX;
+    this->hallarUpper(izq, der, upperIzquierda, upperDerecha);
+
+    int lowerIzquierda = posMasDerechaDeIzqDeX;
+    int lowerDerecha = posMasIzquierdaDeDerDeX;
+    this->hallarLower(izq, der, lowerIzquierda, lowerDerecha);       
+    
     Puntos ret;
-    int ind = uppera;
-    ret.push_back(izq[uppera]);
-    while (ind != lowera) {
-        ind = (ind + 1) % tamanioIzq;
+    int ind = upperIzquierda;
+    ret.push_back(izq[upperIzquierda]);
+    while (ind != lowerIzquierda) {
+        ind = (ind + 1) % (int) izq.size();
         ret.push_back(izq[ind]);
     }
-    ind = lowerb;
-    ret.push_back(der[lowerb]);
-    while (ind != upperb) {
-        ind = (ind + 1) % tamanioDer;
+    ind = lowerDerecha;
+    ret.push_back(der[lowerDerecha]);
+    while (ind != upperDerecha) {
+        ind = (ind + 1) % (int) der.size();
         ret.push_back(der[ind]);
     }
     return ret;
 
 }
 
-void DivisionYConquista::calcularPuntos(std::set<Punto, Comparador> &s, Puntos puntos) {
-    int tamanio = puntos.size();
-    for (int i = 0; i < tamanio; i++) {
-        for (int j = i + 1; j < tamanio; j++) {
-            int a1 = puntos[i].getY() - puntos[j].getY();
-            int b1 = puntos[j].getX() - puntos[i].getX();
-            int c1 = puntos[i].getX() * puntos[j].getY() - puntos[i].getY() * puntos[j].getX();
-            int pos = 0, neg = 0;
-            for (int k = 0; k < tamanio; k++) {
-                if (a1 * puntos[k].getX() + b1 * puntos[k].getY() + c1 <= 0)
-                    neg++;
-                if (a1 * puntos[k].getX() + b1 * puntos[k].getY() + c1 >= 0)
-                    pos++;
-            }
-            if (pos == tamanio || neg == tamanio) {
-                s.insert(puntos[i]);
-                s.insert(puntos[j]);
-            }
-        }
-    }
-}
-
-//void DivisionYConquista::ordenarPuntosAntiHorario(Puntos ret) {
-//    Punto medio;
-//    int n = ret.size();
-//    for (int i = 0; i < n; i++) {
-//        medio.setX(medio.getX() + ret[i].getX());
-//        medio.setY(medio.getY() + ret[i].getY());
-//        ret[i].setX(ret[i].getX() * n);
-//        ret[i].setY(ret[i].getY() * n);
-//    }
-//    sort(ret.begin(), ret.end(), ComparadorDePuntos(medio));
-//    for (int i = 0; i < n; i++) {
-//        ret[i].setX(ret[i].getX() / n);
-//        ret[i].setY(ret[i].getY() / n);
-//    }
-//}
-
-//std::vector<Punto> DivisionYConquista::envolturaBruta(Puntos puntos) {
-//    std::set<Punto, Comparador> s;
-//    Puntos ret;
-//    this->calcularPuntos(s, puntos);
-//    ret.assign(s.begin(), s.end());
-//    this->ordenarPuntosAntiHorario(ret);
-//    return ret;
-//}
-
 std::vector<Punto> DivisionYConquista::dividir(Puntos puntos) {
-    if (puntos.size() < MAXIMO_DE_PUNTOS) {
-//        return this->envolturaBruta(puntos);
+    if (puntos.size() <= MAXIMO_DE_PUNTOS) {
         return puntos;
     }
     int mitad = floor(puntos.size() / 2);
@@ -144,15 +114,9 @@ void DivisionYConquista::generarEnvolturaConvexa() {
     Puntos puntos = this->puntosSeguros;
     std::sort(puntos.begin(), puntos.end(), Comparador());
     Puntos resultado = dividir(puntos);
-    
-    std::cout << "total:" << std::endl;
-    for (auto elem : resultado) {
-        std::cout << elem.getX() << "-" << elem.getY() << std::endl;
+    for (size_t i = 1; i < resultado.size(); i++) {
+        this->envolturaConvexa[resultado[i]] = resultado[i - 1];
     }
-    for (size_t i = 1; i < resultado.size(); i++) {        
-        this->envolturaConvexa[resultado[i-1]] = resultado[i];
-    }
-    this->envolturaConvexa[resultado[resultado.size()]] = resultado[0];
-
+    this->envolturaConvexa[resultado[0]] = resultado[resultado.size() - 1];
 }
 
--- RobotSeguro/DivisionYConquista.h
+++ RobotSeguro/DivisionYConquista.h
@@ -12,12 +12,13 @@ public:
     DivisionYConquista();
     virtual ~DivisionYConquista();
     void generarEnvolturaConvexa();    
-    std::vector<Punto> dividir(Puntos puntos);
-//    std::vector<Punto> envolturaBruta(Puntos puntos);
-    void calcularPuntos(std::set<Punto,Comparador> &s, Puntos puntos);
-//    void ordenarPuntosAntiHorario(Puntos ret);
+    std::vector<Punto> dividir(Puntos puntos);    
     std::vector<Punto> mergearEnvolturas(Puntos izq, Puntos der);
     int cortaElPoligono(Punto a, Punto b, Punto c);
+    int getPosicionMayorX(Puntos puntos);
+    int getPosicionMenorX(Puntos puntos);
+    void hallarUpper(Puntos izq, Puntos der, int &upperIzq, int &upperDer);
+    void hallarLower(Puntos izq, Puntos der, int &lowerIzq, int &lowerDer);
 };
 
 #endif /* DIVISIONYCONQUISTA_H */
